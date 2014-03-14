f4d77bed955630d606ea3aa9698c00b1588fe2ae
Josh Greatrex <jagfin1@gmail.com>
2014-03-14 14:11:32 +0000

    Create ExampleTest.java

---
+++ src/test/java/com/github/example/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.github.example;
+
+import static org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+  @Test
+  public void passingTest() {
+    assertTrue(true);
+  }
+
+}
